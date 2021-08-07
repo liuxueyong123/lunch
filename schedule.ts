5dbab1d1117dea145c115cb98b45ec73d89ee0c2
liuxueyong <liuxueyong@agora.io>
2021-08-07 19:59:48 +0800

    fix: 时间提前到 11.50；提醒只发生在工作日

--- schedule.ts
+++ schedule.ts
@@ -2,7 +2,7 @@ import schedule from 'node-schedule';
 import getTodayLunch from './functions/getTodayLunch';
 import syncWeight from './functions/syncWeight';
 
-schedule.scheduleJob('00 00 12 * * *', () => {
+schedule.scheduleJob('00 50 11 * * 2-6', () => {
   getTodayLunch();
 });
 
