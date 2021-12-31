3094f21c758863e37db1ad7ee51bb29c83458fe0
liuxueyong <liuxueyong@agora.io>
2021-12-31 14:46:57 +0800

    remove lunch

--- schedule.ts
+++ schedule.ts
@@ -1,6 +1,6 @@
 import schedule from 'node-schedule';
-import getTodayLunch from './functions/getTodayLunch';
-import syncWeight from './functions/syncWeight';
+// import getTodayLunch from './functions/getTodayLunch';
+// import syncWeight from './functions/syncWeight';
 import fruitNotice from './functions/fruitNotice';
 import waimaiNotice from './functions/waimai';
 
@@ -8,14 +8,14 @@ schedule.scheduleJob('00 20 11 * * 1-5', () => {
   waimaiNotice();
 });
 
-schedule.scheduleJob('00 50 11 * * 1-5', () => {
-  getTodayLunch();
-});
+// schedule.scheduleJob('00 50 11 * * 1-5', () => {
+//   getTodayLunch();
+// });
 
 schedule.scheduleJob('00 32 14 * * 1-5', () => {
   fruitNotice();
 });
 
-schedule.scheduleJob('05 00 00 * * 1', () => {
-  syncWeight();
-});
+// schedule.scheduleJob('05 00 00 * * 1', () => {
+//   syncWeight();
+// });
