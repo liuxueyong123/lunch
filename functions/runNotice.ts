681f2d30f3512a8c353b741a5dc6f9c27bea39fc
liuxueyong <liuxueyong@agora.io>
2022-07-05 18:25:53 +0800

    fix

--- functions/runNotice.ts
+++ functions/runNotice.ts
@@ -1,7 +1,7 @@
 import { sendArticleToWechat, sendMessageToWechat } from '../extension/wechat';
 
 const runNotice = async () => {
-  await sendArticleToWechat('滴滴滴', '你摸鱼我摸鱼，老板宝马变青桔', 'http://www.agora.io', 'http://lxy520.top/myImage/go-home.jpeg');
+  await sendArticleToWechat('滴滴滴', '你摸鱼我摸鱼，老板宝马变青桔', 'https://996.icu/', 'http://lxy520.top/myImage/go-home.jpeg');
   await sendMessageToWechat('Go Go Go！');
 };
 
