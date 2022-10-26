77db55be843cdc409f74e85a51f893fa68b1f64f
liuxueyong <liuxueyong@agora.io>
2022-10-26 11:13:47 +0800

    修改文案

--- functions/fruitNotice.ts
+++ functions/fruitNotice.ts
@@ -2,7 +2,7 @@ import { sendArticleToWechat, sendMessageToWechat } from '../extension/wechat';
 
 const fruitNotice = async () => {
   await sendArticleToWechat('水果时间到🍉', '做个屁啦，吃水果先啦', 'http://www.agora.io', 'http://lxy520.top/myImage/drink.png');
-  await sendMessageToWechat('吃水果了！');
+  await sendMessageToWechat('');
 };
 
 export default fruitNotice;
