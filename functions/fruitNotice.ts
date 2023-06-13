82cd3752437b6b9c86e686bc1aec831629c32c89
liuxueyong <liuxueyong@agora.io>
2023-06-13 14:23:45 +0800

    重新支持下午水果的通知

--- functions/fruitNotice.ts
+++ functions/fruitNotice.ts
@@ -1,7 +1,7 @@
 import { sendArticleToWechat, sendMessageToWechat } from '../extension/wechat';
 
 const fruitNotice = async () => {
-  await sendArticleToWechat('水果时间到🍉', '做个屁啦，吃水果了', 'http://www.agora.io', 'http://lxy520.top/myImage/drink.png');
+  await sendArticleToWechat('水果时间到🍉', '做个屁啦，吃水果了', 'https://github.com/liuxueyong123/sensitive-word-tool', 'http://lxy520.top/myImage/drink.png');
   await sendMessageToWechat('');
 };
 
--- schedule.ts
+++ schedule.ts
@@ -1,7 +1,7 @@
 import schedule from 'node-schedule';
 // import getTodayLunch from './functions/getTodayLunch';
 // import syncWeight from './functions/syncWeight';
-// import fruitNotice from './functions/fruitNotice';
+import fruitNotice from './functions/fruitNotice';
 import waimaiNotice from './functions/waimai';
 import runNotice from './functions/runNotice';
 // import afternoonTeaNotice from './functions/afternoonTeaNotice';
@@ -14,9 +14,9 @@ schedule.scheduleJob('00 15 11 * * 1-5', () => {
 //   getTodayLunch();
 // });
 
-// schedule.scheduleJob('00 30 14 * * 1-5', () => {
-//   fruitNotice();
-// });
+schedule.scheduleJob('00 30 14 * * 2,5', () => {
+  fruitNotice();
+});
 
 schedule.scheduleJob('00 30 18 * * 1-5', () => {
   runNotice();
